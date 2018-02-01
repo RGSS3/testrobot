5f08e4f08db40d7ff65639c0e7a5d0ec77025f8e
seiran <924172963@qq.com>
2018-02-01 11:49:52 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,10 +6,11 @@ R /^-time/ do
 end
 
 COMMON_DICT = File.read("./etc/rseg/dict/cedict.zh_CN.utf8").split("\n")
-
+require_relative 'zones.rb'
 R /^-diary/ do
   date = `date /t`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
   time = `time /t`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
   weather = %w[阴 晴 雨 雪 大风 霾 以太风]
-  "#{date} #{time} 天气 #{weather.sample}\n#{COMMON_DICT.select{|x| x.size == 2}.sample}"
+  zone = Zones.sample
+  "#{date} #{time} #{zone} 天气 #{weather.sample}\n#{COMMON_DICT.select{|x| x.size == 2}.sample}"
 end
