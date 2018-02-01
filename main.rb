a79a9d0b0999f4168e300c06f130568f85366d6d
seiran <924172963@qq.com>
2018-02-01 10:36:10 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,1 @@
-R /^-time/ do `time /t`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n") end
+R /^-time/ do `time /t`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n") + " " + azogi_text(1) end
