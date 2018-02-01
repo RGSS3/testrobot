7e9c6b279e856d901603dc501f492f4e2237e862
seiran <924172963@qq.com>
2018-02-01 11:09:26 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -5,4 +5,11 @@ R /^-time/ do
   "#{date} #{time} 天气 #{weather.sample}\n#{azogi_text(1)}"
 end
 
+COMMON_DICT = File.read("./etc/rseg/dict/cedict.zh_CN.utf8").split("\n")
 
+R /^-diary/ do
+  date = `date /t`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
+  time = `time /t`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
+  weather = %w[阴 晴 雨 雪 大风 霾 以太风]
+  "#{date} #{time} 天气 #{weather.sample}\n#{COMMON_DICT.select{|x| x.size == 2}.sample}"
+end
